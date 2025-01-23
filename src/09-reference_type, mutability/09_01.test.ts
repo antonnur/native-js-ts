d02672bb9f2a22e469a82af63adb0862a391ce8d
anton_mac <antonnur89@yandex.ru>
2025-01-23 16:49:29 +0700

    +work 09 - Reference type, mutability

---
+++ src/09-reference_type, mutability/09_01.test.ts
@@ -0,0 +1,118 @@
+function increaseAge(u: UserType) {
+  // u.age++
+  u.age = u.age + 1
+}
+
+type UserType = {
+  name: string
+  age: number
+  address: { title: string }
+}
+
+test('Reference type test', () => {
+
+  let user: UserType = {
+    name: 'User',
+    age: 32,
+    address: {
+      title: 'New Ur'
+    }
+  }
+
+  increaseAge(user)
+
+  expect(user.age).toBe(33)
+
+  const superman = user
+
+  superman.age = 1000
+
+  expect(superman.age).toBe(1000)
+})
+
+test('Array reference test', () => {
+
+  let users = [
+    {
+      name: 'User1',
+      age: 32
+    },
+    {
+      name: 'User2',
+      age: 20
+    }
+  ]
+
+  let admins = users
+
+  admins.push({name: 'User3', age: 10})
+
+  expect(users[2]).toEqual({name: 'User3', age: 10})
+})
+
+test('Value type test', () => {
+
+  let usersCount = 100
+
+  let adminsCount = usersCount
+
+  // adminsCount++
+  adminsCount = adminsCount + 1
+
+  expect(usersCount+1).toBe(adminsCount)
+})
+
+test('Reference type test 2', () => {
+
+  const address = {
+    title: 'New Ur'
+  }
+
+  let user: UserType = {
+    name: 'User',
+    age: 32,
+    address: address
+  }
+
+  // let addr = user.address
+
+  let UserNew: UserType = {
+    name: 'Natasha',
+    age: 30,
+    address: address
+  }
+
+  address.title = 'Ur City'
+
+  expect(user.address).toBe(UserNew.address)
+  expect(user.address.title).toBe('Ur City')
+})
+
+test('Reference type array test 3', () => {
+
+  const address = {
+    title: 'New Ur'
+  }
+
+  let user_1: UserType = {
+    name: 'User',
+    age: 32,
+    address: address
+  }
+
+  let user_2: UserType = {
+    name: 'Natasha',
+    age: 30,
+    address: address
+  }
+
+  const users = [user_1, user_2, {name: 'Katya', age: 18, address: address}]
+
+  const admins = [user_1, user_2]
+
+  admins[0].name = 'Tanya'
+
+  expect(users[0].name).toBe('Tanya')
+  expect(user_1.name).toBe('Tanya')
+})
+
